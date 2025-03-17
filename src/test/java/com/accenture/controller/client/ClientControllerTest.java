fecccc920b0f054d576b321497af0044b03c9715
Thébault <elian.thebault@accenture.com>
2025-03-17 15:17:21 +0100

    controller-client-post

--- src/test/java/com/accenture/controller/client/ClientControllerTest.java
+++ src/test/java/com/accenture/controller/client/ClientControllerTest.java
@@ -1,15 +1,36 @@
 package com.accenture.controller.client;
 
-import com.accenture.service.client.ClientService;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import com.accenture.repository.entity.client.Client;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@ExtendWith(MockitoExtension.class)
-public class ClientControllerTest {
-    @Mock
-    ClientService clientService;
-    @InjectMocks
-    ClientController clientController;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ClientControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void testPostClientAvecObjet() throws Exception {
+        Client client = new Client("THEBAULT", "Elian", "elian@mail.com");
+        mockMvc.perform(MockMvcRequestBuilders.post("/clients")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(client)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.id").value(Matchers.not(0)))
+                .andExpect(jsonPath("$.nom").value("THEBAULT"));
+    }
 }
---
+++ src/test/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO CLIENT (nom, prenom, email) VALUES
+('THEBAULT', 'Elian', 'elian@mail.fr'),
+('LOUVART DE PONTLEVOYE', 'Vincent', 'vincent@mail');
