6d39f2d8c1bd2f678af4adb82101ea5f588e9189
Thébault <elian.thebault@accenture.com>
2025-03-17 16:28:40 +0100

    aaa

--- src/test/java/com/accenture/service/ingredient/IngredientServiceImplTest.java
+++ src/test/java/com/accenture/service/ingredient/IngredientServiceImplTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
-public class IngredientServiceImplTest {
+class IngredientServiceImplTest {
     @Mock
     private IngredientDAO ingredientDAO;
     @Mock
